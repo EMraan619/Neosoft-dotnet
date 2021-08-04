6f2c2e79dd5d8a0118a09f451a3bd57344d2f996
EMraan619 <emraanshaikh619@gmail.com>
2021-08-04 13:54:22 +0530

    Hello Program added

---
+++ 01/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Predefined namespace
+using System;
+
+// custom namespace
+//Namespace consist of types (class, interface, delegate,emun ,structs)
+namespace HelloWorld
+{
+    class Program //types consist of type menters (variables, data, methods, events, properties)
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
